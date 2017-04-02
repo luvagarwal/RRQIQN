0652be854aab8c14baa2989eb1c037ef6b2de0b9
Luv Agarwal <luvagarwal.q@gmail.com>
2017-04-02 20:00:03 +0530

    Fix #2

--- utils.py
+++ utils.py
@@ -14,26 +14,33 @@ def diagonalElementsInLatex(level):
         print
         print
 
-# def compact(a):
-#     return collect(a, a.atoms(Symbol), func=factor)
+
+def get_resource_parameters(level=None):
+    if level is not None:
+        return symbols('p%s l%s L%s P%s' % tuple([level + 1] * 4), real=True)
+    return symbols('p l L P', real=True)
+
+def get_basis_parameters(level=None):
+    if level is not None:
+        return symbols('q%s Q%s' % tuple([level + 1] * 2), real=True)
+    return symbols('q Q', real=True)
+
+def get_initial_secret_parameters():
+    return symbols('a b', real=True)
 
 def subsitute2(expr):
-    p, l, q, Q, L, P, b, a = symbols('p l q Q L P, b, a')
+    p, l, L, P = get_resource_parameters()
+    q, Q = get_basis_parameters()
     change = [l, a, p, q, b]
     for sym in change:
         expr = expr.subs(sym*conjugate(sym), Abs(sym)**2)
     return expr
 
 def subsitute(coeff_comm):
-    p, l, q, Q, L, P = symbols('p l q Q L P')
+    p, l, L, P = get_resource_parameters()
+    q, Q = get_basis_parameters()
     coeff_comm = coeff_comm.subs(L, 1/(1+Abs(l)**2)**0.5).subs(P, 1/(1+Abs(p)**2)**0.5).subs(Q, 1/(1+Abs(q)**2)**0.5)
     return coeff_comm
-
-def subsitute3(expr):
-    p, l, q, Q, L, P, b, a = symbols('p l q Q L P, b, a')
-    change = [l, a, p, q, b]
-    for sym in change:
-        expr = expr.subs()
 
 def simplify_density_matrix(density_matrix):
     for row in xrange(4):
