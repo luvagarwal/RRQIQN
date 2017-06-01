72bdffd58a68cece05e6668c49d350124493fb0b
Luv Agarwal <luvagarwal.q@gmail.com>
2017-06-01 08:32:30 +0530

    Some changes

--- analysis_new.py
+++ analysis_new.py
@@ -10,7 +10,7 @@ def calculate_max_dot_product(state):
     state = state.as_single_qubit()
     a, b = state.initial_secret_params
 
-    initial_state = tree.SingleQubit([a, b], state.initial_secret_params, state.resouce_and_basis_params)
+    initial_state = tree.SingleQubit([a, b], state.initial_secret_params, state.resource_and_basis_parameters)
     initial_state.subs_b_by_a()
 
     state.subs_resource_and_basis_params()
@@ -32,12 +32,43 @@ def calculate_max_dot_product(state):
     print initial_state.dot_product(optimized_state)
 
 
+def calculate_max_dot_product_new(state):
+    state = state.as_single_qubit()
+    a, b = state.initial_secret_params
+
+    # Initial secret a|00\ + b|11\
+    initial_state = tree.SingleQubit([a, b], state.initial_secret_params, state.resource_and_basis_parameters)
+    initial_state.subs_b_by_a()
+
+    state.subs_resource_and_basis_params()
+    # state = state.apply_basic_unitary()
+    # state.pprint()
+
+    optimized_state = state.apply_advanced_unitary()
+    # optimized_state.pprint()
+
+    params = state.subs_secret_params()
+    print params
+
+    initial_state.subs_params(params, with_val=True)
+    dp = initial_state.dot_product(state)
+    debug(dp=dp)
+
+    optimized_state.subs_b_by_a()
+    optimized_state.subs_params(params, with_val=True)
+    print initial_state.dot_product(optimized_state)
+
+
 def main():
     root = tree.create_root()
     states = root.get_descendants(3)
-    state = states[random.randint(0, len(states) - 1)]
-    state.pprint()
-    return calculate_max_dot_product(state)
+    # state = states[random.randint(0, len(states) - 1)]
+    # state = states[8]
+    for  state in states:
+        state.pprint()
+        # calculate_max_dot_product_new(state)
+        # print "="*15
+        calculate_max_dot_product(state)
 
 if __name__ == "__main__":
     main()
--- tree.py
+++ tree.py
@@ -47,26 +47,41 @@ class State(object):
 
     def get_children(self, fall_type=None):
         """
-        Return children corresponding to this node.
-        So, it will return array of size 4.
+        Return it's 4 children states after a swapping arranged
+        according to the measurement fall as:
+        [phi_plus, phi_minus, psi_plus, psi_minus]
+
+        Find children nodes is an expensive process specially if
+        we are dealing with too many swappings. So it will only
+        make children when demanded and cache them. It doesn't
+        precalculate them.
         """
         try:
             self.children
         except AttributeError:
             self.children = self.lazy_load_children()
 
-        if fall_type is None:
-            return self.children
+        return self.children
 
+    def get_children_with_fall_type(fall_type):
+        """
+        Return children which is generated when measurement falls
+        to `fall_type`.
+        """
         mapping = {
             'phi_plus': 0,
             'phi_minus': 1,
             'psi_plus': 2,
             'psi_minus': 3,
         }
-        return self.children[mapping[fall_type]]
+
+        return self.get_children()[mapping[fall_type]]
 
     def get_descendants(self, depth):
+        """
+        Return states after swappings = `depth`. So it will
+        return array of size 4^(depth)
+        """
         if depth < 0:
             raise Exception("Depth can't be negative")
         if depth == 0:
@@ -78,12 +93,16 @@ class State(object):
         return descendants
 
     def print_descendants(self, depth):
+        " Pretty states after no of swappings = `depth` "
         descendants = self.get_descendants(depth)
         for state in descendants:
             state.pprint()
 
     def as_density_matrix(self):
-        " Represent this pure state as density matrix "
+        """
+        NOT USED ANYMORE (but works fine)
+        Represent this pure state as density matrix.
+        """
         coeffs = self.get_coefficients_as_general_state()
         matrix = Matrix(coeffs)
         matrix = matrix * matrix.conjugate().transpose()
@@ -92,6 +111,10 @@ class State(object):
         return matrix
 
     def get_density_matrix(self, depth):
+        """
+        Represent all the states after swappings = `depth`
+        as density matrix.
+        """
         nodes = self.get_descendants(depth)
         density_matrix = Matrix([[0]*4 for i in xrange(4)])
 
@@ -102,6 +125,11 @@ class State(object):
         return simplify_density_matrix(density_matrix)
 
     def print_density_matrix(self, depth, only_diagonal=True):
+        """
+        Pretty print density matrix after swappings = `depth`.
+        Since non diagonal elements are zero, it prints only diagonal
+        elements by default.
+        """
         density_matrix = self.get_density_matrix(depth)
         for row in xrange(4):
             for col in xrange(4):
@@ -110,6 +138,9 @@ class State(object):
                 pprint(density_matrix[row, col])
 
     def as_single_qubit(self):
+        """
+        Reconstruct this state to single qubit and return that.
+        """
         a_dash, b_dash = self.coeffs
         norm = sqrt(a_dash**2 + b_dash**2)
         a_dash /= norm
@@ -117,9 +148,6 @@ class State(object):
         p, l, _, _ = self.resource_parameters
         q, _ = self.basis_parameters
         return SingleQubit([a_dash, b_dash], self.initial_secret_params, [p, l, q])
-
-    def get_overlap(self):
-        pass
 
     def pprint(self):
         pprint(self.as_sympy_expr())
@@ -129,7 +157,14 @@ class State(object):
 
 
 class StatePhi(State):
+    """
+    It includes specific functions corresponding to nodes in PHI state i.e. (|00\, |11\)
+    representation. In general, a `State` can either be in  (|00\, |11\) or (|01\, |10\)
+    representation. This class contains functions corresponding to (|00\, |11\)
+    representation.
+    """
     def lazy_load_children(self):
+        " Calculate it's children after a swapping "
         a, b = self.coeffs
         c = self.coeff_comm
         level = self.level
@@ -143,16 +178,32 @@ class StatePhi(State):
         ]
 
     def as_sympy_expr(self):
+        """
+        Used to print this node in a pretty format. It is responsible for those beautiful
+        states that you see on the console.
+        """
         expr = "%s*(%s*Symbol('|00|') + %s*Symbol('|11|'))" % (self.coeff_comm, self.coeffs[0], self.coeffs[1])
         expr = parse_expr(expr)
         return expr
 
     def get_coefficients_as_general_state(self):
+        """
+        Return coefficients assuming this state in
+        x|00\ + y|01\ + z|10\ + w|11\ . Since this is a PHI state,
+        it will have y and z as zero.
+        """
         return [self.coeffs[0], 0, 0, self.coeffs[1]]
 
 
 class StatePsi(State):
+    """
+    It includes specific functions corresponding to nodes in PSI state i.e. (|01\, |10\)
+    representation. In general, a `State` can either be in  (|00\, |11\) or (|01\, |10\)
+    representation. This class contains functions corresponding to (|01\, |10\)
+    representation.
+    """
     def lazy_load_children(self):
+        " Calculate it's children after a swapping "
         a, b = self.coeffs
         c = self.coeff_comm
         level = self.level
@@ -166,26 +217,51 @@ class StatePsi(State):
         ]
 
     def as_sympy_expr(self):
+        """
+        Used to print this node in a pretty format. It is responsible for those beautiful
+        states that you see on the console.
+        """
         expr = "%s*(%s*Symbol('|01|') + %s*Symbol('|10|'))"%(self.coeff_comm, self.coeffs[0], self.coeffs[1])
         expr = parse_expr(expr)
         return expr
 
     def get_coefficients_as_general_state(self):
+        """
+        Return coefficients assuming this state in
+        x|00\ + y|01\ + z|10\ + w|11\ . Since this is a PSI state,
+        it will have x and w as zero.
+        """
         return [0, self.coeffs[0], self.coeffs[1], 0]
 
 
 
 
 class SingleQubit(object):
-    def __init__(self, coeffs, initial_secret_params, resouce_and_basis_params):
+    """
+    This class represents a single qubit state.
+    """
+    def __init__(self, coeffs, initial_secret_params, resource_and_basis_parameters):
+        # [a_dash, b_dash] for (a_dash|0\ + b_dash|1\). Note that for initial
+        # secret a_dash = a and b_dash = b
         self.coeffs = coeffs
+        # [a, b] (initial secret params)
         self.initial_secret_params = initial_secret_params
-        self.resouce_and_basis_params = resouce_and_basis_params
+        # [p, l, q]
+        self.resource_and_basis_parameters = resource_and_basis_parameters
 
     def subs_param(self, param, val):
+        " Substitute `param` = `val` in it's coefficients "
         self.coeffs = [coeff.subs(param, val) for coeff in self.coeffs]
 
     def subs_params(self, params, with_val=False):
+        """
+        If with_val = False (by default) then:
+        - `params` is a list of parameters
+        - Randomly substitute these params. For eg. if we want to substitute p and q randomly,
+         `params` will be [p, q].
+        If with_val = True:
+        - `params` is a dictionary with params as keys and values as their substitution values
+        """
         if not with_val:
             random_params = ParamsSingleton().get_params_values(params)
         else:
@@ -196,33 +272,45 @@ class SingleQubit(object):
         return random_params
 
     def subs_all(self):
+        """
+        Substitute all parameters i.e. [a, b, p, q, q]. Note that b is substituted with
+        (1-a**2)**0.5
+        """
         a, b = self.initial_secret_params
         self.subs_b_by_a()
-        return self.subs_params(self.resouce_and_basis_params + [a])
+        return self.subs_params(self.resource_and_basis_parameters + [a])
 
     def subs_b_by_a(self):
+        " Substitute b by (1-a**2)**0.5 "
         a, b = self.initial_secret_params
         self.subs_param(b, sqrt(1 - a**2))
 
     def subs_secret_params(self):
+        " Substitute a randomly and b accordingly "
         a, b = self.initial_secret_params
         self.subs_b_by_a()
         return self.subs_params([a])
 
     def subs_resource_and_basis_params(self):
-        return self.subs_params(self.resouce_and_basis_params)
+        " Substitute p, q, l randomly "
+        return self.subs_params(self.resource_and_basis_parameters)
 
     def get_abs_coeffs(self):
         abs_coeff = lambda coeff: -1*coeff if -1 in coeff.args else coeff
         return map(abs_coeff, self.coeffs)
 
     def apply_basic_unitary(self):
+        """
+        Convert this state to standard a|0\ + b|1\ state like the way it is done
+        for maximally entangled state after reconstruction.
+        """
         a, b = self.initial_secret_params
         coeffs = self.get_abs_coeffs()
         coeffs = coeffs if a in coeffs[0].args or a is coeffs[0] else coeffs[::-1]
-        return SingleQubit(coeffs, self.initial_secret_params, self.resouce_and_basis_params)
+        return SingleQubit(coeffs, self.initial_secret_params, self.resource_and_basis_parameters)
 
     def apply_advanced_unitary(self):
+        " Apply advanced unitary "
         a, b = self.initial_secret_params
         self.subs_b_by_a()
         b = (1 - a**2) ** 0.5
@@ -240,18 +328,24 @@ class SingleQubit(object):
         debug(x=x)
         y = (1 - x**2) ** 0.5
         optimized_coeffs = [x * a_dash - y * b_dash, y * a_dash + x * b_dash]
-        return SingleQubit(optimized_coeffs, self.initial_secret_params, self.resouce_and_basis_params)
+        return SingleQubit(optimized_coeffs, self.initial_secret_params, self.resource_and_basis_parameters)
 
     def norm(self):
+        " Return it's norm which is square root of sum of it's squared coefficients "
         a, b = self.coeffs
         return (a**2 + b**2) ** 0.5
 
     def dot_product(self, state):
+        " Return dot product of itself with the given `state` "
         a1, b1 = self.coeffs
         a2, b2 = state.coeffs
         return (a1*a2 + b1*b2) / (self.norm() * state.norm())
 
     def as_sympy_expr(self):
+        """
+        Used to print this node in a pretty format. It is responsible for those beautiful
+        states that you see on the console.
+        """
         expr = "%s*Symbol('|0|') + %s*Symbol('|1|')"%(self.coeffs[0], self.coeffs[1])
         expr = parse_expr(expr)
         return expr
@@ -260,12 +354,11 @@ class SingleQubit(object):
         pprint(self.as_sympy_expr())
 
 
-
-def cache(fn):
-    pass
-
 def create_root():
-    " get root node "
+    """
+    Create root node
+    Assumption: we start with a|00\ + b|11\ .
+    """
     a, b = get_initial_secret_parameters()
     return state_factory(TYPES.PHI, 0, [a, b])
 
@@ -275,4 +368,4 @@ def main():
 
 if __name__ == "__main__":
     root = create_root()
-    root.print_density_matrix(2)
+    root.print_descendants(2)
